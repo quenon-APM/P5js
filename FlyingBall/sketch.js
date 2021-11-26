dcbb3be13a474c8b2add8a2866af11ea02e6d6cf
quenon-APM <laurent.quenon@eduhainaut.be>
2021-11-26 22:57:14 +0100

    ajout de commentaires

--- FlyingBall/sketch.js
+++ FlyingBall/sketch.js
@@ -1,8 +1,18 @@
+// déclaration et initialisation
+// variable globale
 let x = 25;
+
+// function setup()
+// executée 1 seul fois 
+// initialisation du canvas
 function setup() {
   createCanvas(400, 400);
 }
 
+// function draw()
+// exécutée en boucle
+// Afin d'offrir l'effet "frame"
+// à chaque "frame" l'affichage est mis à jour
 function draw() {
   background(220);
   circle(x, 200, 50);
